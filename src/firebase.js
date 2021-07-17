54c849e10bd2500011f6410f0c1ee49a0017a978
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-17 21:00:45 +0100

    working on header

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDgaf_5FXs-zhPM2i-3Wsmcap1zD2HD1cY",
-    authDomain: "react-redux-blog-app-cd88b.firebaseapp.com",
-    projectId: "react-redux-blog-app-cd88b",
-    storageBucket: "react-redux-blog-app-cd88b.appspot.com",
-    messagingSenderId: "177719413289",
-    appId: "1:177719413289:web:4cd854ebaa48b679de2a38"
-  };
+  apiKey: "AIzaSyDgaf_5FXs-zhPM2i-3Wsmcap1zD2HD1cY",
+  authDomain: "react-redux-blog-app-cd88b.firebaseapp.com",
+  projectId: "react-redux-blog-app-cd88b",
+  storageBucket: "react-redux-blog-app-cd88b.appspot.com",
+  messagingSenderId: "177719413289",
+  appId: "1:177719413289:web:4cd854ebaa48b679de2a38",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
