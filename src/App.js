630308cd5fe1ceb6fa4f5d67ac74224e2cab8e5e
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-17 21:08:13 +0100

    working on header

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import './App.css';
-import Header from './components/Header/Header';
-import { login, logout } from './features/userSlice';
-import { auth } from './firebase';
-import Blogs from "./components/Blogs/Blogs"
-import { Route } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import Header from "./components/Header/Header";
+import { login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+import Blogs from "./components/Blogs/Blogs";
+import { Route } from "react-router-dom";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-		auth.onAuthStateChanged((authUser) => {
-			if (authUser) {
-				dispatch(
-					login({
-						uid: authUser.uid,
-						photo: authUser.photoURL,
-						email: authUser.email,
-						displayName: authUser.displayName
-					})
-				);
-			} else {
-				dispatch(logout());
-			}
-		});
-	}, [dispatch]);
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
   return (
     <div className="App">
-      <Header/>
+      <Header />
 
-     <Route exact path="/">
-     <div className="app__page">
-        <Blogs/>
-      </div>
-     </Route>
+      <Route exact path="/">
+        <div className="app__page">
+          <Blogs />
+        </div>
+      </Route>
     </div>
   );
 }
