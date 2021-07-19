6fec9111ece068007649de94e9800c41407e8afc
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-19 15:07:12 +0100

    working on add blog

--- src/components/Blogs/AddBlog/AddBlog.js
+++ src/components/Blogs/AddBlog/AddBlog.js
@@ -1,17 +1,30 @@
-import { Button } from '@material-ui/core';
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { selectUser } from '../../../features/userSlice';
-import './AddBlog.css';
-import { useHistory } from 'react-router';
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../../features/userSlice";
+import "./AddBlog.css";
+import { useHistory } from "react-router";
 
 const AddBlog = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [thumbnailUrl, setThumbnailUrl] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [thumbnailUrl, setThumbnailUrl] = useState("");
   const user = useSelector(selectUser);
-	const history = useHistory();
+  const history = useHistory();
 
-}
+  const addBlog = (e) => {
+    e.preventDefault();
+
+    db.collection("blogs").add({
+      title: title,
+      content: content,
+      thumbnailUrl: thumbnailUrl,
+      username: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    history.push("/");
+  };
+};
 
 export default AddBlog;
