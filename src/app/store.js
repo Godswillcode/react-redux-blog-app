24fe3fe8e0d83371d4251fa6acc2c411b637c89f
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-20 21:01:01 +0100

    working on blog display

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
+import blogReducer from '../features/blogSlice';
 
 export const store = configureStore({
   reducer: {
