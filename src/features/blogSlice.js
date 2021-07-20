e71242a2c914c2884f25e579331e5382796d9af1
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-20 20:58:32 +0100

    working on blog display

--- src/features/blogSlice.js
+++ src/features/blogSlice.js
@@ -1,1 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
+
+export const blogSlice = createSlice({
+	name: 'blog',
+	initialState: {
+		blogId: null,
+		blogTitle: null,
+		blogContent: null,
+		blogThumbnailUrl: null,
+		blogAuthor: null
+	},
